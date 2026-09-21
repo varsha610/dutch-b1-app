0caa89b8122c25193a31c6b09634f5122d6f4d50
Varsha Shelar <vshelar610@gmail.com>
2026-09-21 21:56:52 +0200

    Add files via upload
    Initial version of B1 Dutch app

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='b1-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./data.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
